167284f407ad5080fc7693e3f364ada16bcf662d
ozawako1 <ozawako1@gaia.eonet.ne.jp>
2026-04-25 10:50:49 +0900

    Initial Commit

---
+++ DefaultBrowserSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DefaultBrowserSwitcher
+//
+//  Created by ozawako1 on 2026/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DefaultBrowserSwitcher/DefaultBrowserSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultBrowserSwitcherApp.swift
+//  DefaultBrowserSwitcher
+//
+//  Created by ozawako1 on 2026/04/25.
+//
+
+import SwiftUI
+
+@main
+struct DefaultBrowserSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
